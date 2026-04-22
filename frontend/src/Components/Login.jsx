46c1f50efd072ed5b9e7739f191a688c58b77e42
swathi k <swathikbabashankar@gmail.com>
2026-04-22 11:26:21 +0530

    Initial commit

---
+++ frontend/src/Components/Login.jsx
@@ -0,0 +1,16 @@
+
+
+function Login(){
+
+    //login page for google login
+    return(
+        <>
+        <div class="g-signin2" data-onsuccess="onSignIn"></div>
+        // displays the sign in button
+
+        </>
+    )
+
+}
+
+export default Login

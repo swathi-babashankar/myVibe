396bbefedc055220f285f361ffd1aa0f6c003910
swathi k <swathikbabashankar@gmail.com>
2026-04-26 14:33:20 +0530

    Added few styles

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -3,7 +3,8 @@
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
 import './App.css'
-import Navbar from './components/Navbar.jsx'
+import Navbar from './components/Navbar.jsx';
+import VideoDisplay from "./components/VideoDisplay.jsx"
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
       <Navbar/>
      {/* <div className="g-signin2" data-onsuccess="onSignIn"></div> */}
       {/* <div class="g-signin2" data-onsuccess="onSignIn"></div> */}
+      <VideoDisplay/>
     </>
   )
 }
--- frontend/my-app/src/components/VideoDisplay.jsx
+++ frontend/my-app/src/components/VideoDisplay.jsx
@@ -1,16 +1,23 @@
-
+import './styles/videoDisplay.css';
 
 function VideoDisplay(props){
 
 
     return(
         <>
-        <div>
-            <video src={props.video}></video>
-            <h5>{props.channelName}</h5>
-            <h5>{props.views}</h5>
-            <h5>{props.date}</h5>
+        <div className="mainDiv">
+        <div className="vdiv">
+           <iframe className="videoFr" width="311" height="255" src="https://www.youtube.com/embed/Csr2iU8O_rw" title="Control AI suggestions in VSCode" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+           <h5>Control AI suggestions</h5>
+           <span><h5>Chai aur Code</h5>
+           <ul className='channelDet'>
+            <li>500k views</li>
+            <li>1 month ago</li></ul></span>
+        </div>
         </div>
         </>
+        
     )
 }
+
+export default VideoDisplay;

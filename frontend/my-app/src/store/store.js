d9152355e8fc5689d3362ad0b2b856c62312b980
swathi k <swathikbabashankar@gmail.com>
2026-04-28 16:17:29 +0530

    updated store.js

--- frontend/my-app/src/store/store.js
+++ frontend/my-app/src/store/store.js
@@ -1,1 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { userAuthSlice } from "../features/user/userAuthSlice";
 
+
+export const store = configureStore({
+    reducer:{
+        userLogin: userAuthSlice
+    }
+
+})

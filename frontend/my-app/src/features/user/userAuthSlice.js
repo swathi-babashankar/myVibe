dca3732821ace63489ab5beb4013b7dab73b10c7
swathi k <swathikbabashankar@gmail.com>
2026-04-28 16:09:18 +0530

    updated store

---
+++ frontend/my-app/src/features/user/userAuthSlice.js
@@ -0,0 +1,29 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+
+const initialState = {
+    accessToken: null
+};
+
+export const userAuthSlice = createSlice({
+    initialState,
+    reducers: {
+        login: (state, action) =>{
+            state.status = true
+            state.accessToken = action.payload
+
+            action.type = "LOGGEDIN SUCCESSFULLY"
+        },
+
+        logout: (state, action) =>{
+            state.status = false
+            state.accessToken = null
+
+            action.type = "USER_LOGGEDOUT_SUCCESSFULLY"
+
+        }
+    }
+})
+
+export const { login, logout } = userAuthSlice.actions;
+export default userAuthSlice.reducer
